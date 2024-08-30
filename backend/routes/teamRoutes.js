38cd45fcc5417661fb5f2b2d0fd8426f78068621
obada kraishan <obada.kraishan@gmail.com>
2024-08-29 23:55:41 -0500

    Teams Updated

--- backend/routes/teamRoutes.js
+++ backend/routes/teamRoutes.js
@@ -1,14 +1,18 @@
-// File: backend/routes/teamRoutes.js
-
 const express = require('express');
-const { createTeam, getTeams, getTeamById, updateTeam, addComment } = require('../controllers/teamController');
+const {
+  createTeam,
+  getTeams,
+  getTeamById,
+  updateTeam,
+  addComment,
+} = require('../controllers/teamController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 // Routes for managing teams
 router.post('/create', protect, createTeam);
-router.get('/', protect, getTeams);
+router.get('/', protect, getTeams); // This is the GET /teams route
 router.get('/:id', protect, getTeamById);
 router.put('/:id', protect, updateTeam);
 router.post('/:id/comment', protect, addComment);
