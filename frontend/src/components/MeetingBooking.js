0a83ce8fe39192d768586a0d59c4ce557c204df7
obada kraishan <obada.kraishan@gmail.com>
2024-09-03 15:54:23 -0500

    Add Advance Filters

--- frontend/src/components/MeetingBooking.js
+++ frontend/src/components/MeetingBooking.js
@@ -8,6 +8,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Select from 'react-select';
 import Modal from 'react-modal';
 import moment from 'moment';
+import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const statusOptions = [
@@ -17,10 +18,20 @@ const statusOptions = [
   { value: 'canceled', label: 'Canceled' },
 ];
 
+const orderOptions = [
+  { value: 'asc', label: 'Ascending' },
+  { value: 'desc', label: 'Descending' },
+];
+
 const MeetingBooking = () => {
   const [bookings, setBookings] = useState({ bookedWithOthers: [], bookedByOthers: [] });
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedStatus, setSelectedStatus] = useState(null);
+  const [selectedOrder, setSelectedOrder] = useState(orderOptions[0]);
+  const [selectedDepartment, setSelectedDepartment] = useState(null);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [departments, setDepartments] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentBooking, setCurrentBooking] = useState(null);
   const [showMoreWithOthers, setShowMoreWithOthers] = useState(false);
@@ -28,6 +39,20 @@ const MeetingBooking = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
+    const fetchDepartments = async () => {
+      try {
+        const { data } = await api.get('/departments');
+        const departmentOptions = data.map(department => ({
+          value: department._id,
+          label: department.name,
+        }));
+        setDepartments(departmentOptions);
+      } catch (error) {
+        console.error('Error fetching departments:', error);
+        toast.error('Failed to fetch departments.');
+      }
+    };
+
     const fetchBookings = async () => {
       try {
         const user = JSON.parse(localStorage.getItem('userInfo'));
@@ -44,17 +69,41 @@ const MeetingBooking = () => {
         toast.error('Failed to fetch bookings.');
       }
     };
+
+    fetchDepartments();
     fetchBookings();
   }, []);
 
   const handleSearch = (items) => {
-    return items.filter(
-      (item) =>
-        item?.user?.name?.toLowerCase()?.includes(searchTerm.toLowerCase()) ||
-        item?.date?.includes(searchTerm) ||
-        item?.time?.includes(searchTerm) ||
-        item?.agenda?.toLowerCase()?.includes(searchTerm.toLowerCase())
-    );
+    return items.filter((item) => {
+      const matchesSearchTerm = searchTerm
+        ? item?.user?.name?.toLowerCase()?.includes(searchTerm.toLowerCase()) ||
+          item?.date?.includes(searchTerm) ||
+          item?.time?.includes(searchTerm) ||
+          item?.agenda?.toLowerCase()?.includes(searchTerm.toLowerCase())
+        : true;
+
+      const matchesStatus = selectedStatus ? item?.status === selectedStatus.value : true;
+
+      const matchesDepartment = selectedDepartment
+        ? item?.user?.department?.name === selectedDepartment.label
+        : true;
+
+      const matchesDateRange =
+        (startDate && endDate)
+          ? moment(item.date).isBetween(moment(startDate).startOf('day'), moment(endDate).endOf('day'))
+          : true;
+
+      return matchesSearchTerm && matchesStatus && matchesDepartment && matchesDateRange;
+    });
+  };
+
+  const handleOrderChange = (items) => {
+    return items.sort((a, b) => {
+      return selectedOrder.value === 'asc'
+        ? moment(a.date).diff(moment(b.date))
+        : moment(b.date).diff(moment(a.date));
+    });
   };
 
   const handleStatusChange = async (bookingId, newStatus) => {
@@ -90,8 +139,11 @@ const MeetingBooking = () => {
     setCurrentBooking(null);
   };
 
-  const filteredBookedWithOthers = handleSearch(bookings.bookedWithOthers);
-  const filteredBookedByOthers = handleSearch(bookings.bookedByOthers);
+  let filteredBookedWithOthers = handleSearch(bookings.bookedWithOthers);
+  let filteredBookedByOthers = handleSearch(bookings.bookedByOthers);
+
+  filteredBookedWithOthers = handleOrderChange(filteredBookedWithOthers);
+  filteredBookedByOthers = handleOrderChange(filteredBookedByOthers);
 
   // Slice to show only top 5 and conditionally expand
   const visibleBookedWithOthers = showMoreWithOthers
@@ -123,164 +175,205 @@ const MeetingBooking = () => {
           </button>
         </div>
 
-        <div className="mb-4 flex items-center">
-          <FaSearch className="text-gray-500 mr-2" />
-          <input
-            type="text"
-            placeholder="Search meetings..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded-lg"
-          />
+        <div className="mb-4 flex items-center space-x-4">
+          <div className="flex items-center w-1/3">
+            <FaSearch className="text-gray-500 mr-2" />
+            <input
+              type="text"
+              placeholder="Search meetings..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded-lg"
+            />
+          </div>
+          <div className="flex items-center w-1/3">
+            <Select
+              options={statusOptions}
+              value={selectedStatus}
+              onChange={setSelectedStatus}
+              placeholder="Filter by status"
+              className="w-full"
+            />
+          </div>
+          <div className="flex items-center w-1/3">
+            <Select
+              options={departments}
+              value={selectedDepartment}
+              onChange={setSelectedDepartment}
+              placeholder="Filter by department"
+              className="w-full"
+            />
+          </div>
         </div>
 
-        <h3 className="text-xl font-semibold mb-4">Meetings I Have Booked with Others</h3>
-        <Select
-          options={statusOptions}
-          value={selectedStatus}
-          onChange={setSelectedStatus}
-          placeholder="Filter by status"
-          className="mb-4"
-        />
-        {visibleBookedWithOthers.length > 0 ? (
-          <ul>
-            {visibleBookedWithOthers.map((booking) => (
-              <li key={booking._id} className="mb-4 p-4 bg-white rounded-lg shadow-lg relative">
-                <p><strong>Date:</strong> {booking.date}</p>
-                <p><strong>Time:</strong> {booking.time}</p>
-                <p><strong>With:</strong> {booking.user.name}</p>
-                <p><strong>Department:</strong> {booking.user.department.name}</p>
-                <p><strong>Position:</strong> {booking.user.position}</p>
-                <p><strong>Duration:</strong> {booking.duration}</p>
-                <p><strong>Type:</strong> {booking.type}</p>
-                <p><strong>Agenda:</strong> {booking.agenda}</p>
-                <p><strong>Status:</strong> {booking.status}</p>
-                <div className="absolute top-2 right-2 flex space-x-2">
-                  <button
-                    onClick={() => openModal(booking)}
-                    className="bg-yellow-500 text-white p-2 rounded-full hover:bg-yellow-600 transition"
-                  >
-                    <FaEdit />
-                  </button>
-                  <button
-                    onClick={() => handleCancelMeeting(booking._id)}
-                    className="bg-red-500 text-white p-2 rounded-full hover:bg-red-600 transition"
-                  >
-                    <FaTrashAlt />
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No meetings booked with others.</p>
-        )}
-        {filteredBookedWithOthers.length > 5 && !showMoreWithOthers && (
-          <button
-            onClick={() => setShowMoreWithOthers(true)}
-            className="text-blue-500 hover:underline mt-4"
-          >
-            See More
-          </button>
-        )}
+        <div className="mb-4 flex items-center space-x-4">
+          <div className="flex items-center w-1/3">
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              selectsStart
+              startDate={startDate}
+              endDate={endDate}
+              placeholderText="Start Date"
+              className="w-full p-2 border border-gray-300 rounded-lg"
+            />
+          </div>
+          <div className="flex items-center w-1/3">
+            <DatePicker
+              selected={endDate}
+              onChange={(date) => setEndDate(date)}
+              selectsEnd
+              startDate={startDate}
+              endDate={endDate}
+              placeholderText="End Date"
+              className="w-full p-2 border border-gray-300 rounded-lg"
+            />
+          </div>
+          <div className="flex items-center w-1/3">
+            <Select
+              options={orderOptions}
+              value={selectedOrder}
+              onChange={setSelectedOrder}
+              placeholder="Sort by"
+              className="w-full"
+            />
+          </div>
+          </div>
 
-        <h3 className="text-xl font-semibold mt-8 mb-4">Meetings Others Have Booked with Me</h3>
-        <Select
-          options={statusOptions}
-          value={selectedStatus}
-          onChange={setSelectedStatus}
-          placeholder="Filter by status"
-          className="mb-4"
-        />
-        {visibleBookedByOthers.length > 0 ? (
-          <ul>
-            {visibleBookedByOthers.map((booking) => (
-              <li key={booking._id} className="mb-4 p-4 bg-white rounded-lg shadow-lg relative">
-                <p><strong>Date:</strong> {booking.date}</p>
-                <p><strong>Time:</strong> {booking.time}</p>
-                <p><strong>Booked By:</strong> {booking.bookedBy.name}</p>
-                <p><strong>Department:</strong> {booking.bookedBy.department.name}</p>
-                <p><strong>Position:</strong> {booking.bookedBy.position}</p>
-                <p><strong>Duration:</strong> {booking.duration}</p>
-                <p><strong>Type:</strong> {booking.type}</p>
-                <p><strong>Agenda:</strong> {booking.agenda}</p>
-                <p><strong>Status:</strong> {booking.status}</p>
-                <div className="absolute top-2 right-2 flex space-x-2">
-                  <button
-                    onClick={() => openModal(booking)}
-                    className="bg-yellow-500 text-white p-2 rounded-full hover:bg-yellow-600 transition"
-                  >
-                    <FaEdit />
-                  </button>
-                  <button
-                    onClick={() => handleCancelMeeting(booking._id)}
-                    className="bg-red-500 text-white p-2 rounded-full hover:bg-red-600 transition"
-                  >
-                    <FaTrashAlt />
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No meetings booked by others.</p>
-        )}
-        {filteredBookedByOthers.length > 5 && !showMoreByOthers && (
+<h3 className="text-xl font-semibold mb-4">Meetings I Have Booked with Others</h3>
+{visibleBookedWithOthers.length > 0 ? (
+  <ul>
+    {visibleBookedWithOthers.map((booking) => (
+      <li key={booking._id} className="mb-4 p-4 bg-white rounded-lg shadow-lg relative">
+        <p><strong>Date:</strong> {booking.date}</p>
+        <p><strong>Time:</strong> {booking.time}</p>
+        <p><strong>With:</strong> {booking.user.name}</p>
+        <p><strong>Department:</strong> {booking.user.department.name || 'N/A'}</p>
+        <p><strong>Position:</strong> {booking.user.position}</p>
+        <p><strong>Duration:</strong> {booking.duration}</p>
+        <p><strong>Type:</strong> {booking.type}</p>
+        <p><strong>Agenda:</strong> {booking.agenda}</p>
+        <p><strong>Status:</strong> {booking.status}</p>
+        <div className="absolute top-2 right-2 flex space-x-2">
           <button
-            onClick={() => setShowMoreByOthers(true)}
-            className="text-blue-500 hover:underline mt-4"
+            onClick={() => openModal(booking)}
+            className="bg-yellow-500 text-white p-2 rounded-full hover:bg-yellow-600 transition"
           >
-            See More
+            <FaEdit />
           </button>
-        )}
-
-        {/* Edit Meeting Modal */}
-        {currentBooking && (
-          <Modal
-            isOpen={isModalOpen}
-            onRequestClose={closeModal}
-            contentLabel="Edit Meeting"
-            className="bg-white p-6 rounded-lg shadow-lg max-w-lg mx-auto my-20"
+          <button
+            onClick={() => handleCancelMeeting(booking._id)}
+            className="bg-red-500 text-white p-2 rounded-full hover:bg-red-600 transition"
           >
-            <h2 className="text-xl font-semibold mb-4">Edit Meeting</h2>
-            <form>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">Agenda:</label>
-                <input
-                  type="text"
-                  value={currentBooking.agenda}
-                  onChange={(e) => setCurrentBooking({ ...currentBooking, agenda: e.target.value })}
-                  className="w-full p-3 border border-gray-300 rounded-lg"
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2">Status:</label>
-                <Select
-                  options={statusOptions}
-                  value={statusOptions.find(option => option.value === currentBooking.status)}
-                  onChange={(option) => setCurrentBooking({ ...currentBooking, status: option.value })}
-                />
-              </div>
-              <div className="flex justify-end">
-                <button
-                  onClick={() => handleStatusChange(currentBooking._id, currentBooking.status)}
-                  className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition"
-                >
-                  Save Changes
-                </button>
-                <button
-                  onClick={closeModal}
-                  className="bg-gray-500 text-white px-4 py-2 ml-4 rounded-lg hover:bg-gray-600 transition"
-                >
-                  Cancel
-                </button>
-              </div>
-            </form>
-          </Modal>
-        )}
+            <FaTrashAlt />
+          </button>
+        </div>
+      </li>
+    ))}
+  </ul>
+) : (
+  <p>No meetings booked with others.</p>
+)}
+{filteredBookedWithOthers.length > 5 && !showMoreWithOthers && (
+  <button
+    onClick={() => setShowMoreWithOthers(true)}
+    className="text-blue-500 hover:underline mt-4"
+  >
+    See More
+  </button>
+)}
+
+<h3 className="text-xl font-semibold mt-8 mb-4">Meetings Others Have Booked with Me</h3>
+{visibleBookedByOthers.length > 0 ? (
+  <ul>
+    {visibleBookedByOthers.map((booking) => (
+      <li key={booking._id} className="mb-4 p-4 bg-white rounded-lg shadow-lg relative">
+        <p><strong>Date:</strong> {booking.date}</p>
+        <p><strong>Time:</strong> {booking.time}</p>
+        <p><strong>Booked By:</strong> {booking.bookedBy.name}</p>
+        <p><strong>Department:</strong> {booking.bookedBy.department.name || 'N/A'}</p>
+        <p><strong>Position:</strong> {booking.bookedBy.position}</p>
+        <p><strong>Duration:</strong> {booking.duration}</p>
+        <p><strong>Type:</strong> {booking.type}</p>
+        <p><strong>Agenda:</strong> {booking.agenda}</p>
+        <p><strong>Status:</strong> {booking.status}</p>
+        <div className="absolute top-2 right-2 flex space-x-2">
+          <button
+            onClick={() => openModal(booking)}
+            className="bg-yellow-500 text-white p-2 rounded-full hover:bg-yellow-600 transition"
+          >
+            <FaEdit />
+          </button>
+          <button
+            onClick={() => handleCancelMeeting(booking._id)}
+            className="bg-red-500 text-white p-2 rounded-full hover:bg-red-600 transition"
+          >
+            <FaTrashAlt />
+          </button>
+        </div>
+      </li>
+    ))}
+  </ul>
+) : (
+  <p>No meetings booked by others.</p>
+)}
+{filteredBookedByOthers.length > 5 && !showMoreByOthers && (
+  <button
+    onClick={() => setShowMoreByOthers(true)}
+    className="text-blue-500 hover:underline mt-4"
+  >
+    See More
+  </button>
+)}
+
+{/* Edit Meeting Modal */}
+{currentBooking && (
+  <Modal
+    isOpen={isModalOpen}
+    onRequestClose={closeModal}
+    contentLabel="Edit Meeting"
+    className="bg-white p-6 rounded-lg shadow-lg max-w-lg mx-auto my-20"
+  >
+    <h2 className="text-xl font-semibold mb-4">Edit Meeting</h2>
+    <form>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2">Agenda:</label>
+        <input
+          type="text"
+          value={currentBooking.agenda}
+          onChange={(e) => setCurrentBooking({ ...currentBooking, agenda: e.target.value })}
+          className="w-full p-3 border border-gray-300 rounded-lg"
+        />
       </div>
-    </div>
-  );
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2">Status:</label>
+        <Select
+          options={statusOptions}
+          value={statusOptions.find(option => option.value === currentBooking.status)}
+          onChange={(option) => setCurrentBooking({ ...currentBooking, status: option.value })}
+        />
+      </div>
+      <div className="flex justify-end">
+        <button
+          onClick={() => handleStatusChange(currentBooking._id, currentBooking.status)}
+          className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition"
+        >
+          Save Changes
+        </button>
+        <button
+          onClick={closeModal}
+          className="bg-gray-500 text-white px-4 py-2 ml-4 rounded-lg hover:bg-gray-600 transition"
+        >
+          Cancel
+        </button>
+      </div>
+    </form>
+  </Modal>
+)}
+</div>
+</div>
+);
 };
 
 export default MeetingBooking;
+
