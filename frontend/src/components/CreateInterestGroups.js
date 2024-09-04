e666f3c8901aa824fbfa9ebcbd2755c68b6ca45c
obada kraishan <obada.kraishan@gmail.com>
2024-09-04 08:01:20 -0500

    Creat Interes Groups Frontend Enhanced

--- frontend/src/components/CreateInterestGroups.js
+++ frontend/src/components/CreateInterestGroups.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useFormik } from 'formik';
+import Sidebar from './Sidebar';
 import * as yup from 'yup';
 import api from '../utils/api';
 import Select from 'react-select';
