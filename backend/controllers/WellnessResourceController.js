89adc4dc61b05ae2d26e484e3e12da0f33ae61db
obada kraishan <obada.kraishan@gmail.com>
2024-09-11 00:03:04 -0500

    Add Notifications

--- backend/controllers/WellnessResourceController.js
+++ backend/controllers/WellnessResourceController.js
@@ -1,6 +1,27 @@
 const WellnessResource = require('../models/WellnessResourceModel');
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
+const Notification = require('../models/notificationModel');
+const { sendNotification } = require('../services/notificationService');
+
+// Utility function to send notifications to users
+const sendResourceNotifications = async (users, message, link, senderId) => {
+  try {
+    for (const user of users) {
+      const newNotification = new Notification({
+        recipient: user,
+        sender: senderId,
+        message,
+        type: 'info',
+        link,
+      });
+      await newNotification.save();
+      sendNotification(user, newNotification); // Send real-time notification
+    }
+  } catch (error) {
+    console.error('Error sending notifications:', error.message);
+  }
+};
 
 // Create a wellness resource
 const createResource = asyncHandler(async (req, res) => {
@@ -12,9 +33,16 @@ const createResource = asyncHandler(async (req, res) => {
     resourceURL,
     createdBy: req.user._id, // Created by the current user
     createdAt: Date.now(), // Ensure the date is set correctly
-  });  
+  });
 
   console.log("Resource Created:", newResource);
+
+  // Notify all users about the new wellness resource
+  const notificationMessage = `A new wellness resource titled "${resourceTitle}" has been added.`;
+  const allUsers = await User.find().select('_id'); // Fetch all users
+  const userIds = allUsers.map(user => user._id); // Map to user IDs
+  await sendResourceNotifications(userIds, notificationMessage, `/resources/${newResource._id}`, req.user._id);
+
   res.status(201).json(newResource);
 });
 
@@ -53,7 +81,14 @@ const deleteResource = asyncHandler(async (req, res) => {
     throw new Error('Resource not found');
   }
 
-  await WellnessResource.deleteOne({ _id: req.params.resourceId });  // Correct method for deletion
+  await WellnessResource.deleteOne({ _id: req.params.resourceId });
+
+  // Notify users about the deleted resource
+  const notificationMessage = `The wellness resource titled "${resource.resourceTitle}" has been removed.`;
+  const allUsers = await User.find().select('_id'); // Fetch all users
+  const userIds = allUsers.map(user => user._id);
+  await sendResourceNotifications(userIds, notificationMessage, '/resources', req.user._id);
+
   res.status(200).json({ message: 'Resource deleted' });
 });
 
