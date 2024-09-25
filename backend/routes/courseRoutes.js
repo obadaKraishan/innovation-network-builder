d6455ba0018cf3f100322b0e4cad56227d8c0ab9
obada kraishan <obada.kraishan@gmail.com>
2024-09-24 19:56:15 -0500

    Enhance the Course Routes

--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -1,5 +1,16 @@
 const express = require('express');
-const { createCourse, getAllCourses, getCourseById, enrollCourse, submitQuiz, postQuestion } = require('../controllers/courseController');
+const { 
+  createCourse, 
+  getAllCourses, 
+  getCourseById, 
+  enrollCourse, 
+  submitQuiz, 
+  trackProgress, 
+  issueCertificate, 
+  postQuestion, 
+  postAnswer, 
+  upvoteAnswer 
+} = require('../controllers/courseController');
 const { protect, admin } = require('../middleware/authMiddleware');
 
 const router = express.Router();
@@ -9,6 +20,10 @@ router.get('/', protect, getAllCourses);
 router.get('/:id', protect, getCourseById);
 router.post('/enroll/:id', protect, enrollCourse);
 router.post('/quiz/submit', protect, submitQuiz);
+router.get('/progress/:userId/:courseId', protect, trackProgress);
+router.get('/certificate/:userId/:courseId', protect, issueCertificate);
 router.post('/qa/post', protect, postQuestion);
+router.post('/qa/answer', protect, postAnswer);
+router.post('/qa/upvote', protect, upvoteAnswer);
 
 module.exports = router;
